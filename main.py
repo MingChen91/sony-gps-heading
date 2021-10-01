0924d2781490208497aef947843047654a588138
ming <ming.chen@halter.co.nz>
2021-10-01 14:25:06 +1300

    wip

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
+import datetime
+
 import pynmea2
+from matplotlib import pyplot as plt
+
+
+def parse_nmea_messages():
+    nmea_messages = []
+    gps1 = open('data/june-16-moving-test/gps-1.csv', 'r')
+    lines = gps1.readlines()
+
+    for line in lines:
+        try:
+            # remove first character as csv has extra comma
+            nmeaMessage = pynmea2.parse(line.strip()[1:])
+            nmea_messages.append(nmeaMessage)
+        except pynmea2.ParseError as e:
+            print(f'Parse error {e}')
+            continue
+
+    return nmea_messages
+
 
 if __name__ == "__main__":
-    print("hello")
+    nmea_messages = parse_nmea_messages()
 
-pynmea2.parse
+    # draw all times
+    timestamp = []
+    value = []
+    for message in nmea_messages:
+        try:
+            timestamp.append(message.timestamp)
+            value.append(1)
+        finally:
+            continue
+
+    my_date = datetime.date(1991, 7, 15)
+    x_dt = [datetime.datetime.combine(my_date, t) for t in timestamp]
+
+    print(type(timestamp[1]))
+    plt.plot(x_dt, value)
+    plt.show()
---
+++ simple_geo.py
@@ -0,0 +1,60 @@
+import math
+from copy import copy
+
+
+class GeoPostion:
+    def __init__(self, latitude: float, longitude: float):
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def __repr__(self):
+        return f"{self.latitude, self.longitude}"
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class SimpleProjection(object):
+
+    def __init__(self, datum: GeoPostion):
+        self.datum = copy(datum)
+
+    @classmethod
+    def distance(cls, lat1, lon1, lat2, lon2):
+        R = 6371
+        lat_delta = lat2 - lat1
+        long_delta = lon2 - lon1
+        latDistance = math.radians(lat_delta)
+        lonDistance = math.radians(long_delta)
+        a = math.sin(latDistance / 2) * math.sin(latDistance / 2) + math.cos(math.radians(lat1)) * \
+            math.cos(math.radians(lat2)) * \
+            math.sin(lonDistance / 2) * math.sin(lonDistance / 2)
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+        distance = R * c * 1000
+        return distance
+
+    def __call__(self, other: GeoPostion):
+        x = self.distance(self.datum.latitude, other.longitude,
+                          self.datum.latitude, self.datum.longitude)
+        y = self.distance(other.latitude, self.datum.longitude,
+                          self.datum.latitude, self.datum.longitude)
+        if other.longitude < self.datum.longitude:
+            x *= -1
+        if other.latitude < self.datum.latitude:
+            y *= -1
+        return Point(x, y)
+
+
+def cartesion_mag(p):
+    return math.sqrt(p.x*p.x + p.y * p.y)
+
+
+def calculate_delta(p1: GeoPostion, p2: GeoPostion ):
+    projection = SimpleProjection(p1)
+    pp = projection(p2)
+    heading = math.degrees(math.atan2(pp.x, pp.y))
+    return cartesion_mag(pp), heading
+
